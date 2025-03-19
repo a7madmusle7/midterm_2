8f415652ac4ab9258a939f825fbaeb2bacb13c53
a7madmusle7 <50372029+a7madmusle7@users.noreply.github.com>
2025-03-19 11:13:18 -0600

    tiny changes

--- Smart_Watering_draft_a_01/src/Smart_Watering_draft_a_01.cpp
+++ Smart_Watering_draft_a_01/src/Smart_Watering_draft_a_01.cpp
@@ -1,21 +1,27 @@
 /* 
  * Project  Smart Houseplant Watering  System
- * Author:  Ahmad Musleh     @a7madmusle7
- * Date:    Presented on 2025-March-24
+ * Author:  Ahmad Musleh     github @a7madmusle7
+ * Date:    2025-March-24
  */
 
 
 
 #include "Particle.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"
+#include "neopixel.h"
+#include "Colors.h"
 
-SYSTEM_MODE(AUTOMATIC);
+const int OLED_RESET=-1;
+Adafruit_SSD1306 display(OLED_RESET);
 
+SYSTEM_MODE(SEMI_AUTOMATIC);
 SYSTEM_THREAD(ENABLED);
-
-SerialLogHandler logHandler(LOG_LEVEL_INFO);
 
 
 void setup() {
+    Serial.begin(9600);
+    waitFor(Serial.isConnected,10000);
   
 }
 

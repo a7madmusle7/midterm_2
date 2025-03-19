a7586ec92d305e974b8bea71606ef649d2c4a407
a7madmusle7 <50372029+a7madmusle7@users.noreply.github.com>
2025-03-19 10:17:52 -0600

    added some code and updated my proposal

---
+++ Smart_Watering_draft_a_01/src/Smart_Watering_draft_a_01.cpp
@@ -0,0 +1,25 @@
+/* 
+ * Project  Smart Houseplant Watering  System
+ * Author:  Ahmad Musleh     @a7madmusle7
+ * Date:    Presented on 2025-March-24
+ */
+
+
+
+#include "Particle.h"
+
+SYSTEM_MODE(AUTOMATIC);
+
+SYSTEM_THREAD(ENABLED);
+
+SerialLogHandler logHandler(LOG_LEVEL_INFO);
+
+
+void setup() {
+  
+}
+
+
+void loop() {
+  
+}
